5becb11a8379027141d974c87e7cedf2a5a1cdca
Marvin Leiers <marvin.leiers@stud.uni-due.de>
2025-11-09 20:51:53 +0100

    Add pipeline

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -3,11 +3,14 @@ package org.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    void dummy_test() {
+        assertTrue(true, "Dummy test. Should always pass.");
     }
 
 }
