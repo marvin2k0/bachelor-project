c33d14e667a28e55b6860a7951daffc1c26ebe07
Marvin Leiers <marvin.leiers@stud.uni-due.de>
2025-11-11 17:47:31 +0100

    Add start/end-times to Survey and add tests

--- backend/src/main/java/org/example/backend/survey/Survey.java
+++ backend/src/main/java/org/example/backend/survey/Survey.java
@@ -7,16 +7,32 @@ import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Builder
-@Table(name = "Survey")
+@Table(name = "survey")
 public class Survey {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String name;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public boolean isActive() {
+        return isActive(LocalDateTime.now());
+    }
+
+    public boolean isActive(LocalDateTime now) {
+        return (startTime == null || !now.isBefore(startTime)) &&
+                (endTime == null || !now.isAfter(endTime));
+    }
 }
