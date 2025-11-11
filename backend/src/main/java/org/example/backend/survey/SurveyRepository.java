d31d414451bc7400de4ce784b3f26a2ebc81e1e9
noah meister <noah.meister@stud.uni-duisburg-essen.de>
2025-11-11 16:29:00 +0100

    add findByName method to repo and service

--- backend/src/main/java/org/example/backend/survey/SurveyRepository.java
+++ backend/src/main/java/org/example/backend/survey/SurveyRepository.java
@@ -2,5 +2,9 @@ package org.example.backend.survey;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
+
+    Optional<Survey> findByName(String name);
 }
