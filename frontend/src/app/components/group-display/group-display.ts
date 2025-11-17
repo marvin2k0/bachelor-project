9997031b50b1abeabb8f75f20f72bafec0e2d0af
Marvin Leiers <marvin.leiers@stud.uni-due.de>
2025-11-17 16:51:07 +0100

    Add group and input component

---
+++ frontend/src/app/components/group-display/group-display.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-display',
+  imports: [],
+  templateUrl: './group-display.html',
+  styleUrl: './group-display.css',
+})
+export class GroupDisplay {
+
+}
