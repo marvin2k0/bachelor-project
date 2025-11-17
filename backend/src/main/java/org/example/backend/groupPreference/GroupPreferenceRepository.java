c59a28484286af8c00bf1a1b48d142305eeb28c6
noah meister <noah.meister@stud.uni-duisburg-essen.de>
2025-11-17 15:42:34 +0100

    Create GroupPreference package

---
+++ backend/src/main/java/org/example/backend/groupPreference/GroupPreferenceRepository.java
@@ -0,0 +1,6 @@
+package org.example.backend.groupPreference;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupPreferenceRepository extends JpaRepository<GroupPreference, Long> {
+}
