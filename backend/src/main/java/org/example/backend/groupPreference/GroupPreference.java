036478534e2fe7a4ee0d4cdd4e9ac44f2a9c4d6b
noah meister <noah.meister@stud.uni-duisburg-essen.de>
2025-11-17 15:42:34 +0100

    Create GroupPreference package

---
+++ backend/src/main/java/org/example/backend/groupPreference/GroupPreference.java
@@ -0,0 +1,38 @@
+package org.example.backend.groupPreference;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.example.backend.group.Group;
+import org.example.backend.survey.Survey;
+import org.example.backend.user.User;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "GroupPreference")
+public class GroupPreference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "survey_id")
+    private Survey survey;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    private int priority;
+}
