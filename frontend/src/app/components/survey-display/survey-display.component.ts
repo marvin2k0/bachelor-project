44dea0adcbc689310993108fa660fe2b8c32fcb8
Marvin Leiers <marvin.leiers@stud.uni-due.de>
2025-11-20 18:02:24 +0100

    Enhance `survey-display` with image support and styling updates:
    - Add survey placeholder cover image with optimized loading.
    - Update `survey-display` styles for responsiveness and hover effect.
    - Tweak `surveys` and `app-page` layouts for improved spacing and flexibility.
    - Introduce dynamic grid adjustments for smaller survey cards.

--- frontend/src/app/components/survey-display/survey-display.component.ts
+++ frontend/src/app/components/survey-display/survey-display.component.ts
@@ -1,13 +1,14 @@
 import {Component, input} from '@angular/core';
 import {Survey} from '../../model/survey';
 import {TranslocoPipe} from '@jsverse/transloco';
-import {DatePipe} from '@angular/common';
+import {DatePipe, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-survey-display',
   imports: [
     TranslocoPipe,
-    DatePipe
+    DatePipe,
+    NgOptimizedImage
   ],
   templateUrl: './survey-display.component.html',
   styleUrl: './survey-display.component.css'
