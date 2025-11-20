a47134dc91cbb9a413631eed5ba0a9dafcfd0839
Marvin Leiers <marvin.leiers@stud.uni-due.de>
2025-11-20 18:42:57 +0100

    Enhance surveys page layout and localization:
    - Add header and description for surveys page.
    - Update HTML structure with responsive container and description section.
    - Extend `surveys.css` for improved layout styling.
    - Introduce new translation keys for surveys page.

--- frontend/src/app/pages/surveys/surveys.ts
+++ frontend/src/app/pages/surveys/surveys.ts
@@ -1,11 +1,13 @@
 import {Component, computed, inject, signal} from '@angular/core';
 import {SurveyService} from '../../services/survey-service';
 import {SurveyDisplayComponent} from '../../components/survey-display/survey-display.component';
+import {TranslocoPipe} from '@jsverse/transloco';
 
 @Component({
   selector: 'app-surveys',
   imports: [
-    SurveyDisplayComponent
+    SurveyDisplayComponent,
+    TranslocoPipe
   ],
   templateUrl: './surveys.html',
   styleUrl: './surveys.css',
