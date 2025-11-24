cabb35a0d3b0fce631bec532b240e6ac7a0be268
Marvin Leiers <marvin.leiers@stud.uni-due.de>
2025-11-24 11:38:24 +0100

    Enhance modal design flexibility with dynamic color handling and user-select restriction

--- frontend/src/app/services/modal-service.ts
+++ frontend/src/app/services/modal-service.ts
@@ -2,28 +2,34 @@ import {Injectable, signal} from '@angular/core';
 
 export interface ModalSettings {
   title: string,
-  text: string
+  text: string,
+  design?: "red" | "purple"
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private readonly _title = signal("Header")
-  private readonly _text = signal("Something")
+  private readonly _title = signal("")
+  private readonly _text = signal("")
   private readonly _visible = signal<boolean>(true)
+  private readonly _design = signal<"red" | "purple">("purple")
 
   title = this._title.asReadonly()
   text = this._text.asReadonly()
   visible = this._visible.asReadonly()
+  design = this._design.asReadonly()
 
   show(settings: ModalSettings) {
     this._title.set(settings.title)
     this._text.set(settings.text)
+    this._design.set(settings.design ?? "purple")
   }
 
   hide() {
+    this._title.set("")
     this._text.set("")
     this._visible.set(false)
+    this._design.set("purple")
   }
 }
