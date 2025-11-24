3369f6ac36f15b51e64dad2d0f79190baaea097a
Marvin Leiers <marvin.leiers@stud.uni-due.de>
2025-11-24 12:10:23 +0100

    Add accept/deny action handlers to modal component

--- frontend/src/app/services/modal-service.ts
+++ frontend/src/app/services/modal-service.ts
@@ -3,7 +3,9 @@ import {Injectable, signal} from '@angular/core';
 export interface ModalSettings {
   title: string,
   text: string,
-  design?: "red" | "purple"
+  design?: "red" | "purple",
+  onDeny?: () => void,
+  onAccept?: () => void
 }
 
 @Injectable({
@@ -14,11 +16,17 @@ export class ModalService {
   private readonly _text = signal("")
   private readonly _visible = signal<boolean>(true)
   private readonly _design = signal<"red" | "purple">("purple")
+  private readonly _onDeny = signal(() => this.hide())
+  private readonly _onAccept = signal(() => {
+    console.log("accept")
+  })
 
   title = this._title.asReadonly()
   text = this._text.asReadonly()
   visible = this._visible.asReadonly()
   design = this._design.asReadonly()
+  onDeny = this._onDeny.asReadonly()
+  onAccept = this._onAccept.asReadonly()
 
   show(settings: ModalSettings) {
     this._title.set(settings.title)
