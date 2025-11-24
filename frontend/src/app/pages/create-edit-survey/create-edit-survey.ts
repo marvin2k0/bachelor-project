724ae831f2dbba9d5c1e3f06a2810cc4cf14b029
noah meister <noah.meister@stud.uni-duisburg-essen.de>
2025-11-24 17:04:09 +0100

    add form controls for start time, end time, group count, and implement validation logic in create/edit survey page

--- frontend/src/app/pages/create-edit-survey/create-edit-survey.ts
+++ frontend/src/app/pages/create-edit-survey/create-edit-survey.ts
@@ -1,13 +1,13 @@
 import {Component, computed, effect, inject} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {map} from 'rxjs';
 import {Survey} from '../../model/survey';
 import {SurveyService} from '../../services/survey-service';
 import {InputField} from '../../components/input-field/input-field';
 import {ButtonComponent} from '../../components/button/button.component';
 import {TranslocoPipe} from '@jsverse/transloco';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {
   SurveyParticipantImportComponent
 } from '../../components/survey-participant-import/survey-participant-import.component';
@@ -28,11 +28,15 @@ export default class CreateEditSurvey {
   private readonly surveyService = inject(SurveyService)
   private readonly activatedRoute = inject(ActivatedRoute)
   private readonly fb = inject(FormBuilder)
+  private readonly router = inject(Router)
 
   protected readonly form = this.fb.group({
     name: ["", Validators.required],
-    description: [""]
-  })
+    description: [""],
+    startTime: [""],
+    endTime: [""],
+    groupCount: [1, [Validators.min(1)]]
+  }, {validators: this.timeOrderValidator})
 
   protected readonly id = toSignal(
     this.activatedRoute.paramMap.pipe(
@@ -62,5 +66,25 @@ export default class CreateEditSurvey {
         });
       }
     });
+  }
+
+  protected onSubmit() {
+    if (!this.form.invalid) return;
+    this.surveyService.create({name: this.form.value.name!, description: this.form.value.description!, startTime: new Date(this.form.value.startTime!), endTime: new Date(this.form.value.endTime!), groups: [], groupCount: this.form.value.groupCount!})
+      .subscribe({next: () => this.router.navigate(["surveys"])})
+  }
+
+  timeOrderValidator(control: AbstractControl) {
+    const startTime = control.get('startTime')?.value;
+    const endTime = control.get('endTime')?.value;
+
+    if (!startTime || !endTime) {
+      return null;
+    }
+
+    if (startTime < endTime) {
+      return { timeOrder: true };
+    }
+    return null;
   }
 }
